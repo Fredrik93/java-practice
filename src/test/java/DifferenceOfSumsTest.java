34f8100512eafc45a9af9b3fe241d0de629113d8
Fredrik93 <fredrikullman93@gmail.com>
2025-08-15 06:15:40 +0200

    Difference of sums

---
+++ src/test/java/DifferenceOfSumsTest.java
@@ -0,0 +1,19 @@
+import org.example.DifferenceOfSums;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class DifferenceOfSumsTest
+{
+    DifferenceOfSums ds = new DifferenceOfSums();
+    @Test
+    void test10nDivisibleBy3(){
+        int result = ds.differenceOfSums(10, 3);
+        assertThat(result).isEqualTo(19);
+    }
+    @Test
+    void test5nDivisibleBy6(){
+        int result = ds.differenceOfSums(5,6);
+        assertThat(result).isEqualTo(15);
+    }
+}
