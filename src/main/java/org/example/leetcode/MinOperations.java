aec2bdc4fbcf7adfed6956b6cb9a62025ec874ac
Fredrik93 <fredrikullman93@gmail.com>
2025-10-03 07:12:34 +0200

    Exercise

---
+++ src/main/java/org/example/leetcode/MinOperations.java
@@ -0,0 +1,31 @@
+package org.example.leetcode;
+
+public class MinOperations
+{
+    public int minOperations(int[] nums, int k)
+    {
+        // loop over nums
+        // sum nums
+        // base case: if nums % k == 0 then it is divisble, return sum of nums
+        // else subtract from sum until nums % k == 0
+        int sumOfNumbers = 0;
+        int numberOfOperations =0;
+        for(int num : nums)
+        {
+            sumOfNumbers += num;
+        }
+        if(sumOfNumbers % k == 0)
+        {
+            return numberOfOperations;
+        }
+        else
+        {
+            while(sumOfNumbers % k != 0)
+            {
+                numberOfOperations ++;
+                sumOfNumbers--;
+            }
+        }
+        return numberOfOperations;
+    }
+}
