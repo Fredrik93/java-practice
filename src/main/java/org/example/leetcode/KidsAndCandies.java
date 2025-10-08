8541c9c97333efa32377cefa8c88e3bf0b26fa5a
Fredrik93 <fredrikullman93@gmail.com>
2025-10-08 06:42:19 +0200

    set up

---
+++ src/main/java/org/example/leetcode/KidsAndCandies.java
@@ -0,0 +1,11 @@
+package org.example.leetcode;
+
+import java.util.List;
+
+public class KidsAndCandies
+{
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        return List.of(false,false);
+    }
+}
