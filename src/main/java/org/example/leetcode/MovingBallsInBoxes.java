f23f0ee9910420686955b0efcfa9c2c4ead3bb10
Fredrik93 <fredrikullman93@gmail.com>
2025-10-10 06:06:52 +0200

    wip

--- src/main/java/org/example/leetcode/MovingBallsInBoxes.java
+++ src/main/java/org/example/leetcode/MovingBallsInBoxes.java
@@ -1,17 +1,15 @@
 package org.example.leetcode;
 
 /**
- * 1769. Minimum Number of Operations to Move All Balls to Each Box
- * Medium
- * Topics
- * premium lock icon
- * Companies
- * Hint
- * You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+ * 1769. Minimum Number of Operations to Move All Balls to Each Box Medium Topics premium lock icon Companies Hint You
+ * have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1'
+ * if it contains one ball.
  *
- * In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+ * In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1.
+ * Note that after doing so, there may be more than one ball in some boxes.
  *
- * Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+ * Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to
+ * the ith box.
  *
  * Each answer[i] is calculated considering the initial state of the boxes.
  *
@@ -19,8 +17,26 @@ package org.example.leetcode;
  */
 public class MovingBallsInBoxes
 {
-    public int[] minOperations(String boxes) {
+    public int[] minOperations(String boxes)
+    {
+        int[] input = convertToListOfInts(boxes);
 
-        return new int[]{-1};
+        for(int i = 0; i < boxes.length(); i++)
+        {
+            // loop
+
+        }
+        return new int[] { -1 };
+    }
+
+    private int[] convertToListOfInts(String boxes)
+    {
+        char[] charArr = boxes.toCharArray();
+        int[] input = new int[charArr.length];
+        for(int i = 0; i < charArr.length; i++)
+        {
+            input[i] = Integer.parseInt(String.valueOf(charArr[i]));
+        }
+        return input;
     }
 }
