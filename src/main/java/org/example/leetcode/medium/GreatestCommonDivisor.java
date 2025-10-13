4019b3f4698740f900204cd466a5602e964e3799
Fredrik93 <fredrikullman93@gmail.com>
2025-10-13 06:00:58 +0200

    add eerc

--- src/main/java/org/example/leetcode/medium/GreatestCommonDivisor.java
+++ src/main/java/org/example/leetcode/medium/GreatestCommonDivisor.java
@@ -2,18 +2,77 @@ package org.example.leetcode.medium;
 
 public class GreatestCommonDivisor
 {
-    public ListNode insertGreatestCommonDivisors(ListNode head) {
+    public ListNode insertGreatestCommonDivisors(ListNode head)
+    {
+        ListNode resultList = new ListNode(0);
+        ListNode tail = resultList;
+        // loop over listnode head
+        while(head != null && head.next != null)
+        {
+            ListNode current = head;
+            ListNode next = current.next;
 
-        return new ListNode(head.val);
+            System.out.println(current.val + " " + next.val);
+            // find the curr and next nodes value
+            int currVal = current.val;
+            int nextVal = next.val;
+            // find the greatest common divisor
+            int gcd = findGreatestCommonDivisor(currVal, nextVal);
+            // add curr, gcd and next to next listnode result
+            tail.next = new ListNode(currVal);
+            tail = tail.next;
+
+            tail.next = new ListNode(gcd);
+            tail = tail.next;
+
+            head = head.next;
+            // return result
+        }
+        if (head != null) {
+            tail.next = new ListNode(head.val);
+        }
+        return resultList.next;
     }
 
+    private int findGreatestCommonDivisor(int curr, int next)
+    {
+        //gcd can at most be the same as the smallest of curr and next.
+        // e.g., curr = 6 next = 12 then we cant have gcd 12 becase that would mean 6 / 12 = 0.5 which is not an integer
+        int gcd = Math.min(curr, next);
+        while(gcd > 0)
+        {
+            if(curr % gcd == 0 && next % gcd == 0)
+            {
+                return gcd;
+            }
+            else
+            {
+                gcd--;
+            }
 
-      public class ListNode {
-          public int val;
-          public ListNode next;
-          public ListNode() {}
-          public ListNode(int val) { this.val = val; }
-          public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-      }
+        }
+        return gcd;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode()
+        {
+        }
+
+        public ListNode(int val)
+        {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
 
 }
--- src/test/java/leetcode/medium/GreatestCommonDivisorTest.java
+++ src/test/java/leetcode/medium/GreatestCommonDivisorTest.java
@@ -5,14 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class GreatestCommonDivisorTest
-{
+public class GreatestCommonDivisorTest {
+
     private GreatestCommonDivisor gd = new GreatestCommonDivisor();
+
     @Test
-    void testOk(){
-        GreatestCommonDivisor.ListNode node = gd.new ListNode(5);
+    void testOk() {
+        // Input list: 18 -> 6 -> 10 -> 3
+        GreatestCommonDivisor.ListNode node = gd.new ListNode(18);
+        node.next = gd.new ListNode(6);
+        node.next.next = gd.new ListNode(10);
+        node.next.next.next = gd.new ListNode(3);
 
         GreatestCommonDivisor.ListNode res = gd.insertGreatestCommonDivisors(node);
-        assertThat(res.val).isEqualTo(2);
+
+        // Expected list: 18 -> 6 -> 6 -> 2 -> 10 -> 1 -> 3
+        GreatestCommonDivisor.ListNode expected = gd.new ListNode(18);
+        expected.next = gd.new ListNode(6);
+        expected.next.next = gd.new ListNode(6);
+        expected.next.next.next = gd.new ListNode(2);
+        expected.next.next.next.next = gd.new ListNode(10);
+        expected.next.next.next.next.next = gd.new ListNode(1);
+        expected.next.next.next.next.next.next = gd.new ListNode(3);
+
+        // Compare full list
+        assertThat(listToString(res)).isEqualTo(listToString(expected));
+    }
+
+    private String listToString(GreatestCommonDivisor.ListNode node) {
+        StringBuilder sb = new StringBuilder();
+        while (node != null) {
+            sb.append(node.val).append("->");
+            node = node.next;
+        }
+        return sb.toString();
     }
 }
