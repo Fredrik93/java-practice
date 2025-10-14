8266efaf1257df9c19af88d84089e10ce18e011b
Fredrik93 <fredrikullman93@gmail.com>
2025-10-14 05:56:06 +0200

    pivot array

--- src/main/java/org/example/leetcode/medium/PivotArray.java
+++ src/main/java/org/example/leetcode/medium/PivotArray.java
@@ -6,39 +6,37 @@ public class PivotArray
 {
     public int[] pivotArray(int[] nums, int pivot) {
 
-        // figure out where the pivots place should be in the list
-        int [] copy = nums;
-        Arrays.sort(copy);
-        int indexForPivotElement= 0;
-
-        for(int i = 0; i < copy.length ; i++)
-        {
-            if(nums[i] == pivot){
-                // place in list for pivot el found!
-                indexForPivotElement = i;
-
-            }
-        }
 
         // loop over nums
-        // if n i is equal, add to list,
-        // if n i is larger,  add n i (only once)
         int [] result = new int[nums.length];
-        for( int i = 0; i < nums.length; i++){
-            // set the pivot
-            if(i == indexForPivotElement){
-                result[i] = pivot;
-            } else {
-            int currentNum = nums[i];
-            // if n[i] is smaller than pivot, add to new list
-            if(currentNum < pivot){
-                result[i] = currentNum;
+        // keep track of result list
+        int i = 0;
+        // populate list with num < piv
+        for(int num : nums){
+            if(num < pivot){
+                result[i] = num;
+                i++;
             }
-            else if ( currentNum > pivot){
+        }
+        // pop with num == piv
+        for( int num : nums){
+            if(num == pivot)
+            {
+                result[i] = num;
+                i++;
+            }
 
+        }
+        // pop with num > piv
+        for( int num : nums){
+            if(num > pivot)
+            {
+                result[i] = num;
+                i++;
             }
+
         }
-        }
+
         return result;
     }
 }
