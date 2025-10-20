b7d0635fa9257576218b852ac08a80f2f97ff282
Fredrik93 <fredrikullman93@gmail.com>
2025-10-20 06:11:19 +0200

    finished strictly palindromic

--- src/main/java/org/example/leetcode/medium/StrictlyPalindromic.java
+++ src/main/java/org/example/leetcode/medium/StrictlyPalindromic.java
@@ -5,15 +5,25 @@ public class StrictlyPalindromic
     public boolean isStrictlyPalindromic(int n)
     {
         // loop n - 2 times ( inclusive so n = 9 -> 9 - 2 = 7
-        // for each, get the binary representqtion e.g., the number 3  (base 2) -> 011
-        // if representqtion is not palindromic, return false
-        // if never false, return true
-        return false;
+
+        for(int i = 2; i <= n - 2; i++)
+        {
+            String binary = Integer.toString(n, i);
+            StringBuilder sb = new StringBuilder();
+            sb.append(binary);
+            String rev = sb.reverse().toString();
+            if(!binary.equals(rev))
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
 /**
- * An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation of the integer n in base b is palindromic.
+ * An integer n is strictly palindromic if, for every base b between 2 and n - 2 (inclusive), the string representation
+ * of the integer n in base b is palindromic.
  *
  * Given an integer n, return true if n is strictly palindromic and false otherwise.
  *
@@ -23,17 +33,11 @@ public class StrictlyPalindromic
  *
  * Example 1:
  *
- * Input: n = 9
- * Output: false
- * Explanation: In base 2: 9 = 1001 (base 2), which is palindromic.
- * In base 3: 9 = 100 (base 3), which is not palindromic.
- * Therefore, 9 is not strictly palindromic so we return false.
- * Note that in bases 4, 5, 6, and 7, n = 9 is also not palindromic.
- * Example 2:
+ * Input: n = 9 Output: false Explanation: In base 2: 9 = 1001 (base 2), which is palindromic. In base 3: 9 = 100 (base
+ * 3), which is not palindromic. Therefore, 9 is not strictly palindromic so we return false. Note that in bases 4, 5,
+ * 6, and 7, n = 9 is also not palindromic. Example 2:
  *
- * Input: n = 4
- * Output: false
- * Explanation: We only consider base 2: 4 = 100 (base 2), which is not palindromic.
+ * Input: n = 4 Output: false Explanation: We only consider base 2: 4 = 100 (base 2), which is not palindromic.
  * Therefore, we return false.
  *
  *
