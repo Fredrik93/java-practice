fdbfe8ff32e2f7ca37589d83c67dfab854dcdcbf
Fredrik93 <fredrikullman93@gmail.com>
2025-10-26 07:13:44 +0100

    small refactor

--- src/main/java/org/example/leetcode/medium/LongestPalindromicSubstring.java
+++ src/main/java/org/example/leetcode/medium/LongestPalindromicSubstring.java
@@ -10,14 +10,14 @@ public class LongestPalindromicSubstring
         for(int i = 0; i < arr.length; i++)
         {
             StringBuilder sb = new StringBuilder();
-            String reversed;
+            StringBuilder reversed;
             for(int j = i; j < arr.length; j++)
             {
                 char el = arr[j];
                 sb.append(el);
-                reversed = new StringBuilder(sb.toString()).reverse().toString();
+                reversed = new StringBuilder(sb.toString()).reverse();
 
-                if(sb.toString().equals(reversed))
+                if(sb.toString().contentEquals(reversed))
                 {
                     if(sb.toString().length() > longestSubString.length())
                     {
