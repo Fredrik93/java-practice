7710822658ef4f6409a4a6760565f44629c75fa9
Fredrik93 <fredrikullman93@gmail.com>
2025-10-28 06:24:36 +0100

    wip expand around center

--- src/main/java/org/example/leetcode/medium/LongestPalindromicSubstring.java
+++ src/main/java/org/example/leetcode/medium/LongestPalindromicSubstring.java
@@ -4,32 +4,26 @@ public class LongestPalindromicSubstring
 {
     public String longestPalindrome(String s)
     {
+        String result = "";
+        for(int i = 0; i < s.length() ; i++)
+        {
+            if(s.length() % 2 == 0){
+            result = expandAroundCenter(i,i+1, s);}
+            else{
+            result += expandAroundCenter(i, i+1, s);}
+        }
+        return result;
+    }
+
+    private String expandAroundCenter(int left, int right, String s)
+    {
         char arr[] = s.toCharArray();
-        int midInt = arr.length / 2;
-
-        if(arr.length % 2 == 0)
+        while(left >= 0 && right < arr.length && arr[left] == arr[right])
         {
-            // if the array is even we need to start with compqring the two middlemost numbers
-            // a,b,b,c -> start with check if b == b
-            // todo: for now only work with uneven arrs for simplicity
-
+            left--;
+            right++;
         }
-        int ink = 1;
-        for(int i = 0; i < midInt; i++)
-        {
-            //char middle = arr[midInt];
-            int prevIndex = midInt-ink;
-            char prev = arr[prevIndex];
-            int nextIndex = midInt+ink;
-            char next = arr[nextIndex];
-            ink++;
-            if(prev != next){
-
-                // cut out a substring of the selected chars
-                return s.substring(prevIndex, nextIndex);
-            }
-        }
-        return s;
+        return s.substring(left, right);
     }
 }
 
