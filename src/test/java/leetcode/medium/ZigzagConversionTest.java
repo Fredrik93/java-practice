6067cea1402440ce15f0cbcd4ef4885b10003744
Fredrik93 <fredrikullman93@gmail.com>
2025-10-29 06:56:01 +0100

    set up zigzag exercise

---
+++ src/test/java/leetcode/medium/ZigzagConversionTest.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+import org.example.leetcode.medium.ZigzagConversion;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ZigzagConversionTest
+{
+    private ZigzagConversion zc = new ZigzagConversion();
+    @Test
+    void testOk(){
+        String result = zc.convert("PAYPALISHIRING", 3);
+        assertThat(result).isEqualTo("PAHNAPLSIIGYIR");
+    }
+
+    @Test
+    void testOk1(){
+        String result = zc.convert("PAYPALISHIRING", 4);
+        assertThat(result).isEqualTo("PINALSIGYAHRPI");
+    }
+
+    @Test
+    void testOk2(){
+        String result = zc.convert("A", 1);
+        assertThat(result).isEqualTo("A");
+    }
+}
