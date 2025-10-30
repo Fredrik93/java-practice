0b2adb307bd8919ee1e1ad433bcf77cf81f35b65
Fredrik93 <fredrikullman93@gmail.com>
2025-10-30 06:01:04 +0100

    wip

--- src/main/java/org/example/leetcode/medium/ZigzagConversion.java
+++ src/main/java/org/example/leetcode/medium/ZigzagConversion.java
@@ -1,12 +1,12 @@
 package org.example.leetcode.medium;
 
+import java.util.Arrays;
+
 /**
- * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to
+ * display this pattern in a fixed font for better legibility)
  *
- * P   A   H   N
- * A P L S I I G
- * Y   I   R
- * And then read line by line: "PAHNAPLSIIGYIR"
+ * P   A   H   N A P L S I I G Y   I   R And then read line by line: "PAHNAPLSIIGYIR"
  *
  * Write the code that will take a string and make this conversion given a number of rows:
  *
@@ -15,7 +15,21 @@ package org.example.leetcode.medium;
  */
 public class ZigzagConversion
 {
-    public String convert(String s, int numRows) {
-    return null;
+    public String convert(String s, int numRows)
+    {
+        char[] arr = s.toCharArray();
+        // i need q 2 dim array
+        // numrows define how many vertical dimensions i need
+        // try a 2d array with ex1 and start adding chars
+        char [] [] zigzag = new char[numRows][7];
+        print2D(zigzag);
+        boolean h = true;
+        return null;
+    }
+
+    static void print2D(char[][] arr) {
+        for (char[] row : arr) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
