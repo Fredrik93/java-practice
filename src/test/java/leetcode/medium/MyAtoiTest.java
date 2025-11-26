d149c0720e6a19417a8e07b6992b3e0335c3ee5f
Fredrik93 <fredrikullman93@gmail.com>
2025-11-26 05:27:29 +0100

    setup

---
+++ src/test/java/leetcode/medium/MyAtoiTest.java
@@ -0,0 +1,36 @@
+package leetcode.medium;
+
+import org.example.leetcode.medium.MyAtoi;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyAtoiTest
+{
+    private MyAtoi ma = new MyAtoi();
+    @Test
+    void testOk(){
+        int result = ma.myAtoi("42");
+        assertThat(result).isEqualTo(42);
+    }
+    @Test
+    void testOk1(){
+        int result = ma.myAtoi("-042");
+        assertThat(result).isEqualTo(-42);
+    }
+    @Test
+    void testOk3(){
+        int result = ma.myAtoi("1337c0d3");
+        assertThat(result).isEqualTo(1337);
+    }
+    @Test
+    void testOk4(){
+        int result = ma.myAtoi("0-1");
+        assertThat(result).isEqualTo(0);
+    }
+    @Test
+    void testOk5(){
+        int result = ma.myAtoi("words and 987");
+        assertThat(result).isEqualTo(0);
+    }
+}
