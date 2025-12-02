6b0daaea8d93bf7f11efd000a7fce1d62dc99a09
Fredrik93 <fredrikullman93@gmail.com>
2025-12-02 05:30:38 +0100

    gold star 1

--- src/main/java/org/example/adventOfCode2025/Day_1.java
+++ src/main/java/org/example/adventOfCode2025/Day_1.java
@@ -7,10 +7,21 @@ import java.util.List;
 
 public class Day_1
 {
-    // Source - https://stackoverflow.com/a/28219618
-    // Posted by Nery Jr, modified by community. See post 'Timeline' for change history
-    // Retrieved 2025-11-29, License - CC BY-SA 4.0
-
+    /**
+     * The dial starts by pointing at 50.
+     * The dial is rotated L68 to point at 82.
+     * The dial is rotated L30 to point at 52.
+     * The dial is rotated R48 to point at 0.
+     * The dial is rotated L5 to point at 95.
+     * The dial is rotated R60 to point at 55.
+     * The dial is rotated L55 to point at 0.
+     * The dial is rotated L1 to point at 99.
+     * The dial is rotated L99 to point at 0.
+     * The dial is rotated R14 to point at 14.
+     * The dial is rotated L82 to point at 32.
+     * @param args
+     * @throws IOException
+     */
 
     public static void main(String[] args) throws IOException
     {
@@ -22,13 +33,13 @@ public class Day_1
 
     }
     private int rotate (List<String> content){
-        // start at 50
         /**
          * Loop over content, for eqch take apart letter and digit
          * keep limit of 0 - 100
          * if L subtract with digit else if R add
          * if 0 increment counter
          */
+        int ONE_LAP = 100;
         int passCode = 0;
         int current = 50;
 
@@ -37,35 +48,31 @@ public class Day_1
             int ticks = Integer.parseInt(instruction.substring(1));
 
             if(direction == 'L'){
-                // if we reach 0
+                int te = current -ticks;
                 if(current - ticks < 0){
                     ticks -= current;
-                    while(ticks > 99){
-                        ticks -= 99;
+                    while(ticks > 100){
+                        ticks -= ONE_LAP;
                     }
-                    current = 99 - ticks;
+                    current = ONE_LAP - ticks;
                 } else
-                //normal
                 {
                     current -= ticks;
                 }
 
             } else if(direction == 'R'){
-                //todo problem when number is e.g., R777
-                // if we reach 99
-                if(current + ticks > 99){
-                   int remainder = 99 - current;
+                if(current + ticks > 100){
+                   int remainder = 100 - current;
                    ticks -= remainder;
-                   while(ticks > 99){
-                       ticks -= 99;
+                   while(ticks > 100){
+                       ticks -= ONE_LAP;
                    }
                    current = 0;
                    current += ticks;
-                   // normal
                 } else{
                 current += ticks;}
             }
-            if(current == 0){
+            if(current == 0 || current == 100){
                 passCode++;
             }
         }
