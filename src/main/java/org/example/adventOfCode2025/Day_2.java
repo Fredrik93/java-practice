867007a3fd59d0849b824191a7c4db1d38cd07ff
Fredrik93 <fredrikullman93@gmail.com>
2025-12-04 05:49:54 +0100

    dqy 2 test input passes

--- src/main/java/org/example/adventOfCode2025/Day_2.java
+++ src/main/java/org/example/adventOfCode2025/Day_2.java
@@ -8,37 +8,47 @@ public class Day_2
 
     public static void main(String[] args) throws IOException
     {
+        int sumOfInvalidIds = 0;
         List<String> content = ReadInput.readData("src/main/java/org/example/adventOfCode2025/puzzle_input/day2.txt");
         // its just one long line
         String oneLongString = content.getFirst();
         // split by ','
-        String [] ranges  = oneLongString.split(",");
+        String[] ranges = oneLongString.split(",");
 
-        for(String range : ranges){
+        for(String range : ranges)
+        {
             int startOfRange = Integer.parseInt(range.split("-")[0]);
             int endOfRange = Integer.parseInt(range.split("-")[1]);
             int iterations = endOfRange - startOfRange;
             int current = startOfRange;
-            for(int i = 0; i<iterations+1; i++){
+            for(int i = 0; i < iterations + 1; i++)
+            {
                 // check each number for validity rules from the task. including start and end numbers
                 // rule: number shouldnt be repeated twice e.g., 55 or 6464 or 11
                 String s = String.valueOf(current);
-                int splitCurrentFirstHalf = Integer.parseInt(s.substring(0,s.length()/2));
-                int splitCurrentLastHalf = Integer.parseInt(s.substring(s.length()/2));
 
-                if (splitCurrentFirstHalf != splitCurrentLastHalf){
-                    // save some value i think
-                }
-                else {
+                String splitCurrentFirstHalf = s.substring(0, s.length() / 2);
+                String splitCurrentLastHalf = s.substring(s.length() / 2);
+
+                if(splitCurrentFirstHalf.equals(splitCurrentLastHalf))
+                {
+                    if(s.charAt(0) == '0')
+                    {// number shouldnt start with a zero
+                        break;
+                    }
                     System.out.println("invalid number: " + current);
+                    sumOfInvalidIds += current;
+                    System.out.println("Current sumOfInvalidIds is :" + sumOfInvalidIds);
+
                 }
+
                 current++;
 
             }
         }
         Day_2 d = new Day_2();
 
-        System.out.println("Passcode:" );
+        System.out.println("Passcode:");
 
     }
 }
