b3e966621d2b3d8e6a8594a34797954d5f127446
Fredrik93 <fredrikullman93@gmail.com>
2025-12-07 09:42:30 +0100

    refactor

--- src/main/java/org/example/adventOfCode2025/Day3.java
+++ src/main/java/org/example/adventOfCode2025/Day3.java
@@ -2,6 +2,7 @@ package org.example.adventOfCode2025;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,7 +15,7 @@ public class Day3
 
         Day3 d = new Day3();
         int[] sumOfInvalidIds = d.findTwoLargestNumbers(content);
-        int sumNumbers = d.sumNumbers(sumOfInvalidIds);
+        int sumNumbers = Arrays.stream(sumOfInvalidIds).sum();
         System.out.println("result : " + sumNumbers);
 
     }
@@ -22,51 +23,48 @@ public class Day3
     private int[] findTwoLargestNumbers(List<String> content)
     {
         int[] result = new int[content.size()];
-        int resIndex =0;
+        int resIndex = 0;
 
         for(String item : content)
         {
-            // in order, find the highest combination of two digits
-            // 6791829
-            // loop over item
-            // keep current digit
-            // loop over all digits coming after that
-            // keep track of highest number
-            int highest = 0;
+            int highestNum = 0;
             for(int i = 0; i < item.length(); i++)
             {
-                int highestCombination = 0;
-                String firstDigit = String.valueOf(item.charAt(i));
-                if(i == item.length()-1){highestCombination = Integer.parseInt(firstDigit); break;}
-                int secondHighest = 0;
-                for(int j = i + 1; j < item.length(); j++)
+                if(i == item.length() - 1)
                 {
-                    int secondInt = Character.getNumericValue(item.charAt(j));
-                    if(secondInt > secondHighest)
-                    {
-                        secondHighest = secondInt;
-                    }
+                    break;
                 }
-                String res = firstDigit + secondHighest;
-                int current = Integer.parseInt(res);
-                if(current > highest)
-                {
-                    highest = current;
-                }
+                highestNum = getHighest(item, i, highestNum);
             }
-            result[resIndex] = highest;
-            resIndex++;}
+            result[resIndex] = highestNum;
+            resIndex++;
+        }
 
         return result;
     }
 
-    private int sumNumbers(int[] list)
+    private static int getHighest(String item, int i, int highest)
     {
-        int sum = 0;
-        for(int num : list)
+        String firstDigit = String.valueOf(item.charAt(i));
+        int secondDigit = getSecondHighest(item, i);
+        String res = firstDigit + secondDigit;
+        int current = Integer.parseInt(res);
+        highest = Integer.max(current, highest);
+        return highest;
+    }
+
+    private static int getSecondHighest(String item, int i)
+    {
+        int secondHighest = 0;
+
+        for(int j = i + 1; j < item.length(); j++)
         {
-            sum += num;
+            int secondInt = Character.getNumericValue(item.charAt(j));
+            if(secondInt > secondHighest)
+            {
+                secondHighest = secondInt;
+            }
         }
-        return sum;
+        return secondHighest;
     }
 }
