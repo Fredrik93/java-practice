2b5451731b2409e5b10de22218e8bca8bc733c18
Fredrik93 <fredrikullman93@gmail.com>
2025-12-14 14:40:11 +0100

    i got it!

--- src/main/java/org/example/adventOfCode2025/Day5.java
+++ src/main/java/org/example/adventOfCode2025/Day5.java
@@ -1,11 +1,27 @@
 package org.example.adventOfCode2025;
 
 import java.io.IOException;
-import java.math.BigInteger;
 import java.util.*;
 
 public class Day5
 {
+    /**
+     * Paste (replace all info in the txt file) the integers below into the day5.txt file to test a smaller set
+     * The small test set:
+     * 3-5
+     * 10-14
+     * 16-20
+     * 12-18
+     *
+     * 1
+     * 5
+     * 8
+     * 11
+     * 17
+     * 32
+     * @param args
+     * @throws IOException
+     */
 
     public static void main(String[] args) throws IOException
     {
@@ -19,23 +35,27 @@ public class Day5
 
     private long getFreshIngredients(List<String> content)
     {
-        Set<Long> allowedIds = getAllowedIds(content);
+        List<Range> allowedIds = getAllowedRanges(content);
         long[] ingredientsToCheck = getIngredientsToCheck(content);
         return countAllowedIDs(allowedIds, ingredientsToCheck);
     }
-    private List<Range> getAllowedRanges(List<String> content) {
+
+    private List<Range> getAllowedRanges(List<String> content)
+    {
         List<Range> ranges = new ArrayList<>();
-        for (String s : content) {
-            if (s.isEmpty()) break;
+        for(String s : content)
+        {
+            if(s.isEmpty())
+                break;
             String[] parts = s.split("-");
-            ranges.add(new Range(
-                    Long.parseLong(parts[0]),
-                    Long.parseLong(parts[1])
-            ));
+            ranges.add(new Range(Long.parseLong(parts[0]), Long.parseLong(parts[1])));
         }
         return ranges;
     }
-    record Range(long start, long end) {}
+
+    record Range(long start, long end)
+    {
+    }
 
     private Set<Long> getAllowedIds(List<String> content)
     {
@@ -59,41 +79,59 @@ public class Day5
 
     private long[] getIngredientsToCheck(List<String> content)
     {
-        int startOfList = 0;
         long[] ingredients;
         // Figure out how many strings of the list we want
-        for(String c : content)
-        {
-            if(c.isEmpty())
-            {
-                // is c is empty then we found the divider between the different data
-                // +1 because c is currently the empty string, not a number we want
-                startOfList = content.indexOf(c + 1);
-            }
-        }
-        ingredients = new long[content.size() - startOfList];
+        int startIndexOfIngredientsToCheck = findStartOfIngredientsToCheck(content) +1;
+        ingredients = new long[content.size() - startIndexOfIngredientsToCheck];
         int j = 0;
-        for(int i = startOfList; i < content.size(); i++)
+        for(int i = startIndexOfIngredientsToCheck; i < content.size(); i++)
         {
-
-            ingredients[j] = Integer.parseInt(content.get(i));
-            j++;
+            try
+            {
+                long ingredientId = Long.parseLong(content.get(i));
+                ingredients[j] = ingredientId;
+                j++;
+            }
+            catch(IndexOutOfBoundsException iob)
+            {
+                System.out.println("ioby " + iob.getMessage());
+            }
+            catch(NumberFormatException nf)
+            {
+                System.out.println("nfy cause: " + nf.getMessage() + " msg: " + nf.getMessage());
+            }
         }
 
         return ingredients;
     }
 
-    private int countAllowedIDs(Set<Long> allowedIds, long [] ingredientsToCheck)
+    private int findStartOfIngredientsToCheck(List<String> content)
     {
-        int approvedIngredientIDs = 0;
-        for(int i = 0; i < ingredientsToCheck.length; i++)
+        for(String s : content)
         {
-            long currentIngredientId = ingredientsToCheck[i];
-            if(allowedIds.contains(currentIngredientId)){
-                approvedIngredientIDs++;
+            if(s.isEmpty())
+            {
+                return content.indexOf(s);
             }
         }
-        return approvedIngredientIDs;
+        return -1;
+
+    }
+
+    private int countAllowedIDs(List<Range> allowedIds, long[] ingredientsToCheck)
+    {
+        Set <Long> ingredients = new HashSet<>();
+        for(long currentIngredientId : ingredientsToCheck)
+        {
+            for(Range range : allowedIds)
+            {
+                if(currentIngredientId >= range.start && currentIngredientId <= range.end)
+                {
+                    ingredients.add(currentIngredientId);
+                }
+            }
+        }
+        return ingredients.size();
     }
 
 }
