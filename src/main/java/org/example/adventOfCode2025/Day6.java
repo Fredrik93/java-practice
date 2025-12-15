478fe8257911735aab6e537822fa0694043f8f47
Fredrik93 <fredrikullman93@gmail.com>
2025-12-15 05:27:58 +0100

    Day6: wip

--- src/main/java/org/example/adventOfCode2025/Day6.java
+++ src/main/java/org/example/adventOfCode2025/Day6.java
@@ -9,13 +9,9 @@ import java.util.Set;
 public class Day6
 {
     /**
-     * Paste (replace all info in the txt file) the integers below into the day5.txt file to test a smaller set
-     * The small test set:
-     123 328  51 64
-     45 64  387 23
-     6 98  215 314
-     *   +   *   +
-
+     * Paste (replace all info in the txt file) the integers below into the day5.txt file to test a smaller set The
+     * small test set: 123 328  51 64 45 64  387 23 6 98  215 314 +   *   +
+     *
      * @param args
      * @throws IOException
      */
@@ -33,8 +29,31 @@ public class Day6
     private long calulcateChepalopodHomework(List<String> content)
     {
         // construct 2d array
-        // loop over arr and get the ith column, process that ie perform the arithmetic operator thats in the same column
+        int[][] arrayOfNumbers = construct2dArray(content);
 
-      return -1;
-       }
+        // loop over arr and get the ith column, process that ie perform the arithmetic operator thats in the same column. return result of ith column operation
+        int calculcateSumOfOneColumn = calculateSumOfOneColumn(arrayOfNumbers);
+
+        // loop over all columns and do the same thing, summing them together. return the sum
+        int sumAllColumns = sumColumns(arrayOfNumbers);
+        return -1;
+    }
+
+    private int[][] construct2dArray(List<String> content)
+    {
+        return new int[][] {};
+    }
+
+    private int calculateSumOfOneColumn(int[][] arrayOfNumbers)
+    {
+
+        return -1;
+    }
+
+    private int sumColumns(int[][] arrayOfNumbers)
+    {
+
+        return 0;
+    }
+
 }
