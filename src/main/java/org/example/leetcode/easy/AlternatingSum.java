03da805740e0d2e50b7ad50efc91350fef8d56b3
Fredrik93 <fredrikullman93@gmail.com>
2026-01-09 05:24:21 +0100

    Task complete

--- src/main/java/org/example/leetcode/easy/AlternatingSum.java
+++ src/main/java/org/example/leetcode/easy/AlternatingSum.java
@@ -3,7 +3,15 @@ package org.example.leetcode.easy;
 public class AlternatingSum
 {
     public int alternatingSum(int[] nums) {
-
-        return -1;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            int curr = nums[i];
+            if(i % 2 == 0){
+                sum += curr;
+            } else {
+                sum -= curr;
+            }
+        }
+        return sum;
     }
 }
