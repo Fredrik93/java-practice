35bd27459c88281d3f31be704c959ddc16feba9d
Fredrik93 <fredrikullman93@gmail.com>
2026-01-24 17:00:59 +0100

    all tests passing

--- src/main/java/org/example/leetcode/easy/BitFlip.java
+++ src/main/java/org/example/leetcode/easy/BitFlip.java
@@ -7,8 +7,9 @@ public class BitFlip
         //convert to binary representation of the start int
         // same for the goal
         // compare each i in start and goal, if they are not the same then count the difference. return the difference, which will be the steps required to get the goal
-        String startBinary = Integer.toBinaryString(start);
-        String goalBinary = Integer.toBinaryString(goal);
+        String startBinary = buildBinaryNumber(start);
+        String goalBinary = buildBinaryNumber(goal);
+
         int countFlips = 0;
         for(int i = 0; i < goalBinary.length(); i++)
         {
@@ -21,4 +22,15 @@ public class BitFlip
         }
         return countFlips;
     }
+
+    private static String buildBinaryNumber(int bits)
+    {
+        StringBuilder binary = new StringBuilder(Integer.toBinaryString(bits));
+        int amountOfZerosToAppend = 30 - Integer.toBinaryString(bits).length();
+        for(int i = 0; i < amountOfZerosToAppend; i++)
+        {
+            binary.insert(0,0);
+        }
+        return binary.toString();
     }
+}
--- src/test/java/leetcode/easy/BitFlipTest.java
+++ src/test/java/leetcode/easy/BitFlipTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BitFlipTest
 {
-    private BitFlip bf = new BitFlip();
+    private final BitFlip bf = new BitFlip();
     @Test
     void test(){
         int res = bf.minBitFlips(10,7);
@@ -17,5 +17,15 @@ public class BitFlipTest
     void test1(){
         int res = bf.minBitFlips(3,4);
         assertThat(res).isEqualTo(3);
+    }
+    @Test
+    void test2(){
+        int res = bf.minBitFlips(234, 640);
+        assertThat(res).isEqualTo(5);
+    }
+    @Test
+    void test3(){
+        int res = bf.minBitFlips(9155,3670);
+        assertThat(res).isEqualTo(8);
     }
 }
