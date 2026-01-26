be76656449699edc4eddd12a274fdf8aac5388eb
Fredrik93 <fredrikullman93@gmail.com>
2026-01-26 05:43:26 +0100

    wip

--- src/main/java/org/example/leetcode/easy/LargestLocalMatrix.java
+++ src/main/java/org/example/leetcode/easy/LargestLocalMatrix.java
@@ -4,14 +4,31 @@ import org.example.helpers.MatrixHelpers;
 
 public class LargestLocalMatrix
 {
-    public int[][] largestLocal(int[][] grid) {
+    public int[][] largestLocal(int[][] grid)
+    {
+
+        String s = null;
 
         MatrixHelpers.printMatrixInAPrettyWay(grid);
+        try
+        {
+            int arit = 2 / 0;
+        }
+        catch(ArithmeticException a)
+        {
+            System.out.println("arith");
+        }
 
+        try
+        {
+            int l = s.length();
+        }
+        catch(NullPointerException e)
+        {
+            throw new ArrayIndexOutOfBoundsException(String.valueOf(e));
+        }
+        System.out.println("here");
         return grid;
     }
-
-
-
 
 }
