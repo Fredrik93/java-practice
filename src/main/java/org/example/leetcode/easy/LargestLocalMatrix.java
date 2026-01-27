bf411be883b089ccf0b3520901fc121da68727be
Fredrik93 <fredrikullman93@gmail.com>
2026-01-27 06:23:37 +0100

    wip

--- src/main/java/org/example/leetcode/easy/LargestLocalMatrix.java
+++ src/main/java/org/example/leetcode/easy/LargestLocalMatrix.java
@@ -12,13 +12,17 @@ public class LargestLocalMatrix
         // check all elements that are at most i+1 or j + 1 steps away, that way you check the closest neighbours
         int lengthToIterate = grid[0].length * 2;
         // for test 1 should be four elemented 6,2,2,6 (the middlemost elements)
-        int [] allMddleElementsInMatrix = new int[]{};
-        for(int i = 0; i<4; i++){
-            int middleElement =0;
-            for(int j = 0; j < 4; j++)
+        int[] allMddleElementsInMatrix = new int[4];
+        int k = 0;
+        // gte a list of middleelements. not sure if i need this but i could verify that im getting the mid elements i need at least
+        for(int i = 0; i < 2; i++)
+        {
+            int middleElement = 0;
+            for(int j = 0; j < 2; j++)
             {
-                middleElement = grid[i+1][j+1];
-                allMddleElementsInMatrix[j] = middleElement;
+                middleElement = grid[i + 1][j + 1];
+                allMddleElementsInMatrix[k] = middleElement;
+                k++;
             }
         }
 
