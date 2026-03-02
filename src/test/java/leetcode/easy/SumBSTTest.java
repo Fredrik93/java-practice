19fc4f59b92e4152bb7635a7325e0b2bf0f972c0
Fredrik93 <fredrikullman93@gmail.com>
2026-03-02 05:36:18 +0100

    wip

---
+++ src/test/java/leetcode/easy/SumBSTTest.java
@@ -0,0 +1,14 @@
+package leetcode.easy;
+
+import org.example.leetcode.easy.SumBST;
+import org.junit.jupiter.api.Test;
+
+public class SumBSTTest {
+    private SumBST bst = new SumBST();
+    SumBST.TreeNode tn = bst. new TreeNode();
+
+    @Test
+    void testOK(){
+        int res = bst.rangeSumBST(tn, 7,15);
+    }
+}
