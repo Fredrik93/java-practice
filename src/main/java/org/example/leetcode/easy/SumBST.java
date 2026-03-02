754095a332e86807d1563e2426a86c081c5a5043
Fredrik93 <fredrikullman93@gmail.com>
2026-03-02 05:50:15 +0100

    wip

--- src/main/java/org/example/leetcode/easy/SumBST.java
+++ src/main/java/org/example/leetcode/easy/SumBST.java
@@ -5,23 +5,6 @@ import java.util.Queue;
 
 public class SumBST {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        return -1;
+      return -1;
     }
-
-
-
-
-    public class TreeNode {
-          int val;
-          TreeNode left;
-          TreeNode right;
-          public TreeNode() {}
-          TreeNode(int val) { this.val = val; }
-          TreeNode(int val, TreeNode left, TreeNode right) {
-              this.val = val;
-              this.left = left;
-              this.right = right;
-          }
-      }
-
 }
--- src/test/java/leetcode/easy/SumBSTTest.java
+++ src/test/java/leetcode/easy/SumBSTTest.java
@@ -1,14 +1,22 @@
 package leetcode.easy;
 
 import org.example.leetcode.easy.SumBST;
+import org.example.leetcode.easy.TreeNode;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class SumBSTTest {
     private SumBST bst = new SumBST();
-    SumBST.TreeNode tn = bst. new TreeNode();
-
     @Test
     void testOK(){
-        int res = bst.rangeSumBST(tn, 7,15);
+// [10,5,15,3,7,null,18] as a tree
+        TreeNode root = new TreeNode(10,
+            new TreeNode(5, new TreeNode(3), new TreeNode(7)),
+            new TreeNode(15, null, new TreeNode(18))
+        );
+
+        int res = bst.rangeSumBST(root, 7, 15);
+        assertThat(res).isEqualTo(32);
     }
 }
