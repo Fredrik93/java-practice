7294bce153a6dbe5ee6b098c8f2e3c95558ee51f
Fredrik93 <fredrikullman93@gmail.com>
2026-03-05 05:47:43 +0100

    wip tests passing

--- src/main/java/org/example/leetcode/easy/SumBST.java
+++ src/main/java/org/example/leetcode/easy/SumBST.java
@@ -5,22 +5,37 @@ import java.util.Queue;
 
 public class SumBST {
     public int rangeSumBST(TreeNode root, int low, int high) {
-       // iterate the tree
-        int sum =0;
+        // iterate the tree
+        int sum = levelOrder(root, low, high);
+
+        // if between low or high add to sum
+        // return
+        return sum;
+    }
+
+    // Breadth first search
+    public int levelOrder(TreeNode root, int low, int high) {
+        if (root == null) return -1;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            int currVal = current.val;
 
-        while (!queue.isEmpty()){
-            TreeNode curr = queue.poll();
-            if(curr.left != null){
-                queue.add(curr.left);
+            if (currVal >= low && currVal <= high) {
+                sum += currVal;
             }
-            if(curr.right != null){
-                queue.add(curr.right);
+            if (current.left != null) {
+                queue.add(current.left);
             }
+
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+
         }
-       // if between low or high add to sum
-       // return
-      return -1;
+        return sum;
     }
+
 }
