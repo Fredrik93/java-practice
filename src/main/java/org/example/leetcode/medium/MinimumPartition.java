a5457d16a81533b2708db99532ee9e330936565c
Fredrik93 <fredrikullman93@gmail.com>
2026-03-10 05:49:50 +0100

    wip

--- src/main/java/org/example/leetcode/medium/MinimumPartition.java
+++ src/main/java/org/example/leetcode/medium/MinimumPartition.java
@@ -2,7 +2,20 @@ package org.example.leetcode.medium;
 
 public class MinimumPartition {
     public int minPartitions(String n) {
+        int num = Integer.parseInt(n);
+        int operations = 0;
+        int multiplier = 1;
 
-        return -1;
+        while (num > 0) {
+
+            if (num % 10 != 0) {
+                num -= 11 * multiplier;
+                operations++;
+            } else {
+                num -= 10 * multiplier;
+                operations++;
+            }
+        }
+        return operations;
     }
 }
