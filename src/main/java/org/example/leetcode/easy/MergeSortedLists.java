e4f3f69ad45f595e7be5d6659999bb8bb8f8bc76
Fredrik93 <fredrikullman93@gmail.com>
2026-03-14 11:14:11 +0100

    why are nodes so difficult to understand

--- src/main/java/org/example/leetcode/easy/MergeSortedLists.java
+++ src/main/java/org/example/leetcode/easy/MergeSortedLists.java
@@ -4,9 +4,16 @@ import org.example.leetcode.medium.GreatestCommonDivisor;
 
 public class MergeSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        
+        ListNode current = list1;
+        while (current != null) {
+            current = current.next;
+            current.next = list2;
 
+        }
         return list1;
-
     }
 
 
