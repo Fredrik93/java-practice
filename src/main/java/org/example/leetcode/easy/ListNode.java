97c5307a21bfc44babba8e785150f4bebafe3a7a
Fredrik93 <fredrikullman93@gmail.com>
2026-03-16 05:58:14 +0100

    wip

--- src/main/java/org/example/leetcode/easy/ListNode.java
+++ src/main/java/org/example/leetcode/easy/ListNode.java
@@ -6,4 +6,14 @@ public class ListNode {
     public ListNode() {}
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        if (val != listNode.val) return false;
+        if (next == null) return listNode.next == null;
+        return next.equals(listNode.next);
+    }
 }
--- src/main/java/org/example/leetcode/easy/MergeSortedLists.java
+++ src/main/java/org/example/leetcode/easy/MergeSortedLists.java
@@ -6,7 +6,7 @@ public class MergeSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
         if (list2 == null) return list1;
-        
+
         ListNode current = list1;
         while (current != null) {
             current = current.next;
