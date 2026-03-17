528eaa68c8b5dbc56f17b270b9d2c832df18eb7f
Fredrik93 <fredrikullman93@gmail.com>
2026-03-17 06:58:29 +0100

    passing

--- src/main/java/org/example/leetcode/easy/BitwiseXOR.java
+++ src/main/java/org/example/leetcode/easy/BitwiseXOR.java
@@ -2,6 +2,15 @@ package org.example.leetcode.easy;
 
 public class BitwiseXOR {
     public int xorOperation(int n, int start) {
-        return -1;
+        int sum = 0;
+        int [] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = start + 2 * i;
+        }
+
+        for (int num: nums){
+            sum ^= num;
+        }
+        return sum;
     }
 }
