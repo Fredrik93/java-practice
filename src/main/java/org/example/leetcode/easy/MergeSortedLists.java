169e65b215b26ad2d53b91ab304f58b8d3a54ceb
Fredrik93 <fredrikullman93@gmail.com>
2026-03-19 05:52:35 +0100

    wip

--- src/main/java/org/example/leetcode/easy/MergeSortedLists.java
+++ src/main/java/org/example/leetcode/easy/MergeSortedLists.java
@@ -2,6 +2,7 @@ package org.example.leetcode.easy;
 
 import org.example.leetcode.medium.GreatestCommonDivisor;
 
+//Should learn this
 public class MergeSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
@@ -9,9 +10,10 @@ public class MergeSortedLists {
 
         ListNode current = list1;
         while (current != null) {
+            System.out.println("list 1: " + current.val);
+            System.out.println("list 2 :" +  list2.val);
             current = current.next;
-            current.next = list2;
-
+            list2 = list2.next;
         }
         return list1;
     }
