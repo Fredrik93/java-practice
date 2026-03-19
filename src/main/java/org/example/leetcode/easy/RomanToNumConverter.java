c0eb408f9f946a2bbcab79f5794d84ed79a2390e
Fredrik93 <fredrikullman93@gmail.com>
2026-03-19 05:49:10 +0100

    wip

--- src/main/java/org/example/leetcode/easy/RomanToNumConverter.java
+++ src/main/java/org/example/leetcode/easy/RomanToNumConverter.java
@@ -7,18 +7,22 @@ public class RomanToNumConverter {
         //  convert the char to number
         int currentSymbolSum = 0;
         int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             char currentChar = arr[i];
+            char nextChar = arr[i+1];
             // if I then iterate until no longer an I and convert to number, eg IIV gives 1 (I) and then 4 (IV) or IIIV gives 2 (II) and 4 (IV)
             switch (currentChar) {
                 case 'I':
-                    currentSymbolSum++;
+                    currentSymbolSum += calcISymbol(nextChar);
+                    sum += currentSymbolSum;
                     break;
                 case 'V':
                     currentSymbolSum = 5;
+                    sum += currentSymbolSum;
                     break;
                 case 'X':
                     currentSymbolSum = 10;
+                    sum += currentSymbolSum;
                     break;
             }
 
@@ -26,5 +30,15 @@ public class RomanToNumConverter {
         sum += currentSymbolSum;
 
         return sum;
+    }
+
+    private int calcISymbol(char nextChar) {
+
+        int curr = 0;
+        if(nextChar == 'I'){
+            curr++;
+        }
+
+        return curr;
     }
 }
