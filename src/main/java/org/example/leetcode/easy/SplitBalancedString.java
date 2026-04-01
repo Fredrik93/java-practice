3f66df2488623b69dc8c717d899f595edfc5a4df
Fredrik93 <fredrikullman93@gmail.com>
2026-04-01 06:05:05 +0200

    wip

--- src/main/java/org/example/leetcode/easy/SplitBalancedString.java
+++ src/main/java/org/example/leetcode/easy/SplitBalancedString.java
@@ -9,22 +9,20 @@ public class SplitBalancedString {
         // if next is the same as curr R == R then just continue
         // if curr == R and next == L then you have a subpar, count that
         int balanceSum = 0;
-
+        int balanceR = 0;
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
             char balance = s.charAt(i);
-
             if (balance == 'L') {
                 balanceSum++;
-            } else {
+            } else if (balance == 'R') {
                 if (balanceSum > 0) {
                     balanceSum--;
                 }
-                if (balanceSum == 0){
+                if (balanceSum == 0) {
                     result++;
                 }
             }
-
 
         }
         return result;
