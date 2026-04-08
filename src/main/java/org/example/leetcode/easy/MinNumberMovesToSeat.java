381eb0a1853a8fa31f556f50995bc028512b5bc3
Fredrik93 <fredrikullman93@gmail.com>
2026-04-08 05:36:48 +0200

    wip

--- src/main/java/org/example/leetcode/easy/MinNumberMovesToSeat.java
+++ src/main/java/org/example/leetcode/easy/MinNumberMovesToSeat.java
@@ -6,25 +6,30 @@ public class MinNumberMovesToSeat {
         int sumOfSteps = 0;
         // for each student, go over the seats and find the smallest diff
 
-        for (int i = 0; i < students.length; i++) {
+        for (int student : students) {
             int shortestPathToASeat = Integer.MAX_VALUE;
+            int indexOfSeat = 0;
+
             for (int j = 0; j < seats.length; j++) {
-                int student = students[i];
 
-                if(seats[j] != -1){
-                int seat = seats[j];
+                if (seats[j] != -1) {
+                    int seat = seats[j];
 
-                int currentStepsNeededToSeatStudent = Math.abs(student - seat);
-                if(currentStepsNeededToSeatStudent < shortestPathToASeat){
-                    shortestPathToASeat = currentStepsNeededToSeatStudent;
-                    // block the seat for other students
-                    seats[j] = -1;
+                    int currentStepsNeededToSeatStudent = Math.abs(student - seat);
+                    if (currentStepsNeededToSeatStudent < shortestPathToASeat) {
+                        shortestPathToASeat = currentStepsNeededToSeatStudent;
+                        indexOfSeat = j;
+                    }
+
+
                 }
-            }
+
 
             }
-
+            // block the seat for other students
+            seats[indexOfSeat] = -1;
             sumOfSteps += shortestPathToASeat;
+
         }
         // remove that seat or block it after a student is seated
         // sum up the smallest diffs and return
