717bffdce6a77a20d703193e31982ce460a68362
Fredrik93 <fredrikullman93@gmail.com>
2026-04-11 10:24:01 +0200

    Tests passing

--- src/main/java/org/example/leetcode/medium/DeciNumbers.java
+++ src/main/java/org/example/leetcode/medium/DeciNumbers.java
@@ -2,6 +2,28 @@ package org.example.leetcode.medium;
 
 public class DeciNumbers {
     public int minPartitions(String n) {
-        return -1;
+        // figure out the highest number that can be used eg for 32 it is 11, next is 100 which is larger than 32
+        int numberOfOperations = 0;
+        int number = 0;
+        while(number >= 0) {
+            number -= calculateHighestIntegerThatCanBeDeducedFromTotal(n);
+            numberOfOperations++;
+            System.out.println("number: " + number);
+
+            // deduce that from sum
+            //  count operations
+        }
+
+
+        return numberOfOperations;
     }
+    private int calculateHighestIntegerThatCanBeDeducedFromTotal(String n){
+        int numberOfDigits = n.length();
+
+
+        return 1;
+    }
+
+
+
 }
