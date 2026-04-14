c096029838533a6e3da8c23fb05035b9dd7a20fc
Fredrik93 <fredrikullman93@gmail.com>
2026-04-14 05:46:06 +0200

    comments

--- src/main/java/org/example/leetcode/medium/MergeNodeBetweenZeros.java
+++ src/main/java/org/example/leetcode/medium/MergeNodeBetweenZeros.java
@@ -2,8 +2,10 @@ package org.example.leetcode.medium;
 
 public class MergeNodeBetweenZeros {
     public ListNode mergeNodes(ListNode head) {
-        Hi hi = new Hi();
-        int h = hi.getH();
+        //iterate over nodes and check for 0
+        // if 0 is found, start summing integers until 0 is found again
+        // add sum as a node to a new ListNode
+        // return new ListNode as result
         return null;
     }
 
@@ -21,17 +23,6 @@ public class MergeNodeBetweenZeros {
         public ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
-        }
-    }
-
-    public class Hi {
-        int h;
-
-        public Hi() {
-        }
-
-        public int getH() {
-            return 2;
         }
     }
 }
