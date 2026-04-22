8a3f0dcb9e2a74166dbd30e36f04637157d04762
Fredrik93 <fredrikullman93@gmail.com>
2026-04-22 06:01:14 +0200

    sort array

--- src/main/java/org/example/leetcode/easy/WideVerticalArea.java
+++ src/main/java/org/example/leetcode/easy/WideVerticalArea.java
@@ -1,7 +1,14 @@
 package org.example.leetcode.easy;
 
+import java.util.Arrays;
+
 public class WideVerticalArea {
     public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
+
+        for(int [] row : points){
+            System.out.println(Arrays.toString(row));
+        }
 return -1;
     }
 }
