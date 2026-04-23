c04ae965b0365ca9454cadefe67deb832adb3121
Fredrik93 <fredrikullman93@gmail.com>
2026-04-23 06:07:01 +0200

    passes

--- src/main/java/org/example/leetcode/easy/WideVerticalArea.java
+++ src/main/java/org/example/leetcode/easy/WideVerticalArea.java
@@ -5,10 +5,17 @@ import java.util.Arrays;
 public class WideVerticalArea {
     public int maxWidthOfVerticalArea(int[][] points) {
         Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
-
-        for(int [] row : points){
-            System.out.println(Arrays.toString(row));
+        int biggestDifference = 0;
+        for(int i = 0; i < points.length-1; i++){
+            int current = points[i][0];
+            int next = points[i+1][0];
+            System.out.println("Compareing ****");
+            System.out.println(current);
+            System.out.println(next);
+            System.out.println("******");
+            int diff = next - current;
+            biggestDifference = Math.max(diff, biggestDifference);
         }
-return -1;
+return biggestDifference;
     }
 }
