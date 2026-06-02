5b7380a4fe5989039469e9f98e9e3d96e42ade0d
Fredrik93 <fredrikullman93@gmail.com>
2026-06-02 07:40:05 +0200

    bfs

--- src/main/java/org/example/algorithms/BreadthFirstSearch.java
+++ src/main/java/org/example/algorithms/BreadthFirstSearch.java
@@ -4,20 +4,40 @@ import java.util.*;
 
 public class BreadthFirstSearch {
 
-    public Node bfs(Node start){
+    public List<Node> bfs(Node start) {
+        List <Node> result = new ArrayList<>();
         // should print nodes in bfs order
+        // Add set of visited nodes so that it wont loop forever if theres a cycle
+        Set <Node> visited = new HashSet<>();
+
+        // 1. create empty queue and add the first node (start node)
         Queue<Node> queue = new ArrayDeque<>();
-        Node node = start;
-        while(node != null){
-            queue.add(node);
+        queue.add(start);
+
+        // 2 while queue is not empty take the first node out and print the name
+        while (!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if(!visited.contains(currentNode)) {
+                System.out.println("the node is: " + currentNode.name);
+                result.add(currentNode);
+                // 3. for each neighbor, add it to the queue
+                for(Node n: currentNode.neighbors){
+                    queue.add(n);
+                }
+            }
+            visited.add(currentNode);
+
 
         }
-        return start;
+        return result;
     }
+
     public class Node {
-        String name;
+        public String name;
         public List<Node> neighbors = new ArrayList<>();
 
-        public Node(String name) {this.name = name;}
+        public Node(String name) {
+            this.name = name;
+        }
     }
 }
--- src/test/java/algorithms/BreadthFirstSearchTest.java
+++ src/test/java/algorithms/BreadthFirstSearchTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BreadthFirstSearchTest {
     private BreadthFirstSearch breadthFirstSearch = new BreadthFirstSearch();
 
@@ -31,8 +33,35 @@ public class BreadthFirstSearchTest {
         BreadthFirstSearch.Node f = bfs.new Node("F");
         c.neighbors.add(f);
 
+        List<BreadthFirstSearch.Node> result = breadthFirstSearch.bfs(a);
+        List<String> actualNodeNames = result.stream().map(n -> n.name).toList();
+        List<String> expectedNodeNames = List.of("A", "B", "C", "D", "E", "F");
 
-        breadthFirstSearch.bfs(a);
+        assertEquals(expectedNodeNames, actualNodeNames);
+    }
+
+    /**
+     * If there is a cycle we should make sure that it does not loop forever
+     */
+    @Test
+    void testCycleisOk() {
+        BreadthFirstSearch bfs = new BreadthFirstSearch();
+
+        BreadthFirstSearch.Node a = bfs.new Node("A");
+        BreadthFirstSearch.Node b = bfs.new Node("B");
+        // A -> B
+        a.neighbors.add(b);
+
+        // B -> A
+        b.neighbors.add(a);
+
+        List<BreadthFirstSearch.Node> result = breadthFirstSearch.bfs(a);
+        List<String> actualNodeNames = result.stream().map(n -> n.name).toList();
+        List<String> expectedNodeNames = List.of("A", "B");
+
+        assertEquals(expectedNodeNames, actualNodeNames);
+
+
     }
 
 
